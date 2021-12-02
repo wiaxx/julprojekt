477dea075a46daa62c8905876766a3244fa19a4f
Amanda <91470242+wiaxx@users.noreply.github.com>
2021-12-02 11:21:27 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const adminCred = {
-        username: "admin",
-        password: "password"
-    };
-    if (localStorage.getItem("admin") === null) {
-        localStorage.setItem("admin", JSON.stringify(adminCred));
-    }
-});
